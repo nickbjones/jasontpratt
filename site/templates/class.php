b03a4e89f9e8c41b6c2b6cb68fa304a269514b73
Nick Jones <jonesnickb@gmail.com>
2018-02-12 14:27:20 +0900

    building views

--- site/templates/class.php
+++ site/templates/class.php
@@ -1,7 +1,5 @@
 <?php snippet('header') ?>
-
   <main class="main" role="main">
-    
     <header class="wrap">
       <h1><?= $page->title()->html() ?></h1>
       <div class="intro text">
@@ -9,25 +7,9 @@
       </div>
       <hr />
     </header>
-    
     <div class="text wrap">
-      
       <?= $page->text()->kirbytext() ?>
-
-      <?php
-      // Images for the "class" template are sortable. You
-      // can change the display by clicking the 'edit' button
-      // above the files list in the sidebar.
-      foreach($page->images()->sortBy('sort', 'asc') as $image): ?>
-        <figure>
-          <img src="<?= $image->url() ?>" alt="<?= $page->title()->html() ?>" />
-        </figure>
-      <?php endforeach ?>
-      
     </div>
-    
-    <?php snippet('prevnext') ?>
-
+    <?php // snippet('prevnext') ?>
   </main>
-
 <?php snippet('footer') ?>
